88969ffdcd4403b1b5cd20e32aed37d6631bd1c5
Link4Electronics <link4electronics@gmail.com>
2026-02-18 23:19:59 -0300

    add WMCLASS

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,6 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/dethrace.desktop
+export STARTUPWMCLASS=dethrace
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
